dc06cb402f1f9fbb7409280ab4ecdc9b36a2d997
ashaaut <ashaautade1998@gmail.com>
2021-03-12 10:54:00 +0530

    added basic arithmatic operations

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
+
 public class Calculator {
 
    public static void main(String[] args) {
-      System.out.println("hiii");
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter the first Number: ");
+      int num1=sc.nextInt();
+      System.out.println("Enter the second Number: ");
+      int num2=sc.nextInt();
+      System.out.println("Choose operation between (+,-,*,/): ");
+      String operation=sc.next();
+      switch(operation){
+         case "+":
+         System.out.print("Sum="+(num1+num2));
+         break;
+         case "-":
+         System.out.print("Substraction="+(num1-num2));
+         break;
+         case "*":
+         System.out.print("Multiplicatiom="+(num1*num2));
+         break;
+         case "/":
+         System.out.print("Dividation="+(num1/num2));
+         break;
+         default:
+         System.out.print("Invalid Operation");
+      }
    }
 }
