08543101d00cb7ee546f7d2f0e1aafc8a7f7ea82
ashaaut <ashaautade1998@gmail.com>
2021-03-13 18:27:25 +0530

    added program for printing last character in string

---
+++ TryCatch.java
@@ -0,0 +1,9 @@
+public class TryCatch{
+    public static void main(String [] args){
+        String str="Welcome";
+        char [] chars=str.toCharArray();
+        char lastChar=chars[chars.length];
+        System.out.println("last char: "+lastChar);
+    
+    }
+}
