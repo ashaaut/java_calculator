a905536392ea5f68665055b56caa5b31eb90011d
ashaaut <ashaautade1998@gmail.com>
2021-03-13 18:34:29 +0530

    added try-catch block for handling exception

--- TryCatch.java
+++ TryCatch.java
@@ -1,9 +1,13 @@
 public class TryCatch{
     public static void main(String [] args){
+        try{
         String str="Welcome";
         char [] chars=str.toCharArray();
         char lastChar=chars[chars.length];
         System.out.println("last char: "+lastChar);
+        } catch(Exception e){
+            e.printStackTrace();
+        }
     
     }
 }
